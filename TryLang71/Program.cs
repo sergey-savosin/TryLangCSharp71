44a9a7d6eb7910b3513016b2a383640aaf70ff36
Sergey Savosin <savosin_sergey@mail.ru>
2017-11-06 15:19:12 +0300

    Добавьте файлы проекта.

---
+++ TryLang71/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace TryLang71
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var results = Enumerable.Range(1, 40)
+                .Select(input => (input, task: FibonacciAsync(input)))
+                .ToArray();
+
+            foreach (var tuple in results)
+            {
+                WriteLine($"Fib {tuple.input} = {await tuple.task}");
+            }
+        }
+
+        private static Task<int> FibonacciAsync(int n, CancellationToken token = default)
+        {
+            return Task.Run(() => Fib(n).curr, token);
+
+            (int curr, int prev) Fib(int i)
+            {
+                if (i is 0) return (1, 0);
+                var (c, p) = Fib(i - 1);
+                return (c + p, c);
+            }
+        }
+
+    }
+}
